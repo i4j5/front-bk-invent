01e9e3303c2028b3a3fd633a4f020603f0362d5f
Aleksandr Sazhin <i4j5@ya.ru>
2019-07-03 16:29:51 +0300

    -

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -211,17 +211,20 @@ $(function() {
             controls: false,
             auto: false,
             pager: false,
-            // adaptiveHeight: true,
-            // adaptiveHeightSpeed: 100
+            adaptiveHeight: true,
+            adaptiveHeightSpeed: 400
         })
     
         let quantity =$quiz.slider.getSlideCount()
         let i = 1
+
     
         let $progressBar = $quiz.find('.quiz__progress-bar')
         $progressBar._label = $progressBar.children('.quiz__progress-bar-label')
         $progressBar._field = $progressBar.children('.quiz__progress-bar-field').children('span')
-    
+        let $number = $quiz.find('.quiz__number')
+        $number.html(`${i}-й вопрос из ${quantity-1}`)
+
         // Сделать Автоматическое далее !!!
         $quiz.find('.quiz__next').click(function() {
             
@@ -230,12 +233,12 @@ $(function() {
 
                 let $input = $quiz.find(`[name="comment[${i}]"]`)
                 // Проверка чекбоксоы 
-                console.log($input, $input.val())
 
                 if(i+1 == quantity ) {
                     $progressBar._field.css('transition', 'none')
                     $quiz.find('.quiz__footer').css({
                         visibility: 'hidden',
+                        display: 'none',
                         opacity: '0'
                     })
                 }
@@ -243,6 +246,7 @@ $(function() {
                 $progressBar._field.css('width', `${parseInt(100/(quantity-1)*i)}%`)
                 $progressBar._label.html(`${parseInt(100/(quantity-1)*i)}%`)
                 ++i
+                $number.html(`${i}-й вопрос из ${quantity-1}`)
                 
             } 
         })
@@ -253,10 +257,21 @@ $(function() {
                 --i
                 $progressBar._field.css('width', `${parseInt(100/(quantity-1)*(i-1))}%`)
                 $progressBar._label.html(`${parseInt(100/(quantity-1)*(i-1))}%`)
+                $number.html(`${i}-й вопрос из ${quantity-1}`)
             }  
         })
 
         $('.run-quiz').click(function() {
+            if($(window).width() >= 700) {
+                $quiz.slider.reloadSlider({
+                    touchEnabled: false,
+                    controls: false,
+                    auto: false,
+                    pager: false,
+                    adaptiveHeight: false
+                })
+            }
+
             $quiz.slider.goToSlide(0)
             $progressBar._field.css({
                 transition: 'none',
@@ -265,9 +280,11 @@ $(function() {
             $progressBar._label.html('0%')
             $quiz.find('.quiz__footer').css({
                 visibility: 'visible',
+                display: '',
                 opacity: '1'
             })
             i = 1
+            $number.html(`${i}-й вопрос из ${quantity-1}`)
             $(`#${$(this).data('quiz')}`).openModal()
             $quiz.slider.reloadSlider()
             $progressBar._field.css('transition', '')
@@ -277,10 +294,6 @@ $(function() {
 
 
     
-
-    
-
-
 
 
 
