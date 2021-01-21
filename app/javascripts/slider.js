495637436decccaf7c2e9cc216f63fe304195a78
Aleksandr Sazhin <i4j5@ya.ru>
2021-01-21 16:58:25 +0300

    =

--- app/javascripts/slider.js
+++ app/javascripts/slider.js
@@ -10,7 +10,7 @@ $(function() {
         // prevSelector: 'slider__prev',
         minSlides: 1,
         maxSlides: 4,
-        slideWidth: 270,
+        slideWidth: 271,
         pager: false,
     })
 
