38de02128db8e4d56545b5994a1ec9e6c2710032
Aleksandr Sazhin <i4j5@ya.ru>
2021-01-22 16:08:12 +0300

    -

--- app/javascripts/slider.js
+++ app/javascripts/slider.js
@@ -2,7 +2,7 @@ const $ = require('jquery')
 
 $(function() {
 
-    $('.slider__items').bxSlider({
+    $('.slider_type_cases .slider__items').bxSlider({
         touchEnabled: false,
         nextText: '',
         prevText: '',
@@ -11,7 +11,14 @@ $(function() {
         minSlides: 1,
         maxSlides: 4,
         slideWidth: 271,
-        pager: false,
+        pager: true,
+    })
+
+    $('.slider_type_objects .slider__items').bxSlider({
+        touchEnabled: false,
+        nextText: '',
+        prevText: '',
+        pager: true,
     })
 
 
