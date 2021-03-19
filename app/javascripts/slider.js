a4be8805ee68cf69c96db70f9c03511117210497
Aleksandr Sazhin <i4j5@ya.ru>
2021-03-19 16:31:14 +0300

    -

--- app/javascripts/slider.js
+++ app/javascripts/slider.js
@@ -6,16 +6,18 @@ $(function() {
         touchEnabled: false,
         nextText: '',
         prevText: '',
-        // nextSelector: '.slider__next',
-        // prevSelector: 'slider__prev',
+        // preventDefaultSwipeX: false,
+        // preventDefaultSwipeY: false,
         minSlides: 1,
         maxSlides: 4,
         slideWidth: 271,
         pager: true,
+        stopAutoOnClick: true,
     })
 
     $('.slider_type_objects .slider__items').bxSlider({
         touchEnabled: false,
+        
         nextText: '',
         prevText: '',
         pager: true,
