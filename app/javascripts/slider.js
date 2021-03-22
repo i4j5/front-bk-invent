80bebc7461ddba1a1076c5df435864e63437044e
Aleksandr Sazhin <i4j5@ya.ru>
2021-03-22 17:50:35 +0300

    -

--- app/javascripts/slider.js
+++ app/javascripts/slider.js
@@ -2,25 +2,46 @@ const $ = require('jquery')
 
 $(function() {
 
+    // $('.slider_type_objects .slider__items').bxSlider({
+    //     touchEnabled: false,
+    //     nextText: '',
+    //     prevText: '',
+    //     minSlides: 1,
+    //     maxSlides: 4,
+    //     slideWidth: 271,
+    //     pager: true,
+    //     stopAutoOnClick: true,
+    // })
+
+    $('.slider_type_feedbacks .slider__items').bxSlider({
+        touchEnabled: false,
+        nextText: '',
+        prevText: '',
+        minSlides: 1,
+        maxSlides: 3,
+        slideWidth: 368,
+        pager: true,
+        stopAutoOnClick: true,
+        onSlideBefore: function() {
+            $('.feedback__text_open').removeClass('feedback__text_open')
+        }
+    })
+
     $('.slider_type_cases .slider__items').bxSlider({
         touchEnabled: false,
         nextText: '',
         prevText: '',
-        // preventDefaultSwipeX: false,
-        // preventDefaultSwipeY: false,
-        minSlides: 1,
-        maxSlides: 4,
-        slideWidth: 271,
         pager: true,
-        stopAutoOnClick: true,
     })
 
-    $('.slider_type_objects .slider__items').bxSlider({
-        touchEnabled: false,
-        
-        nextText: '',
-        prevText: '',
-        pager: true,
+    $('.feedback__more>a').click(function() {
+        let $this = $(this)
+        $this.parent().parent().addClass('feedback__text_open')
+    })
+
+    $('.feedback__dide>a').click(function() {
+        let $this = $(this)
+        $this.parent().parent().parent().removeClass('feedback__text_open')
     })
 
 
